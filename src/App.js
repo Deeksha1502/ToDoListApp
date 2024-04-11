849b4ba4b68709cf081b5b07ecb0080f519044be
Deeksha <deekshad1502@gmail.com>
2024-04-11 15:03:03 +0530

    feat/add and delete todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import { ToDo } from './components/ToDo';
 
 export const App = () => {
-  return (
-    <div className='text-4xl text-center m-5 pb-10'>
-      To Do List
-      <ToDo />
-    </div>
-  );
+  return <ToDo />;
 };
